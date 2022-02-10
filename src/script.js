51fc00e54a50c776530dc9d3add65759d08ce1ee
sunny kyung <sunny.kyung88@gmail.com>
2022-02-11 02:23:04 +0900

    particles

--- src/script.js
+++ src/script.js
@@ -1,73 +1,68 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-
-const textureLoader = new THREE.TextureLoader()
-
-const tex1 = textureLoader.load("/textures/door/color.jpg")
-const tex2 = textureLoader.load("/textures/door/alpha.jpg")
-const tex3 = textureLoader.load("/textures/door/ambientOcclusion.jpg")
-const tex4 = textureLoader.load("/textures/door/height.jpg")
-const tex5 = textureLoader.load("/textures/door/normal.jpg")
-const tex6 = textureLoader.load("/textures/door/metalness.jpg")
-const tex7 = textureLoader.load("/textures/door/roughness.jpg")
-
-const tex8 = textureLoader.load("/textures/matcaps/1.png")
-const tex9 = textureLoader.load("/textures/gradients/3.jpg")
+import * as dat from 'lil-gui'
 
 /**
  * Base
  */
+// Debug
+const gui = new dat.GUI()
+
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
 
-//const material = new THREE.MeshBasicMaterial()
-// material.color= new THREE.Color('red')
-// material.map = tex1
-// material.color = new THREE.Color(0x00ff00)
-// material.wireframe = true
-// material.transparent= true
-// material.opacity = 0.5
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
+const particleTexture = textureLoader.load("/textures/particles/2.png")
 
-// const material = new THREE.MeshNormalMaterial()
-// material.wireframe = true
+// const particlesGeometry = new THREE.SphereGeometry(1,32,32)
+// const particlesMaterial = new THREE.PointsMaterial()
+// particlesMaterial.size = 0.02
+// particlesMaterial.sizeAttenuation = true
+//
+// const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+// scene.add(particles)
 
-// const material = new THREE.MeshMatcapMaterial()
-// material.matcap = tex8
+const particlesGeometry = new THREE.BufferGeometry()
+const count = 20000
 
-const material = new THREE.MeshDepthMaterial()
+const positions = new Float32Array(count * 3)
+const colors = new Float32Array(count *3)
 
-const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5,16,16),
-    material)
+for(let i =0; i < positions.length; i++) {
+    positions[i] = (Math.random() - 0.5) * 10
+    colors[i] = Math.random()
+}
 
-sphere.position.x = -1.5
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions,3))
+particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colors,3))
 
-const plane = new THREE.Mesh(
-    new THREE.PlaneBufferGeometry(1,1),
-    material
-)
+const particlesMaterial = new THREE.PointsMaterial()
+particlesMaterial.size = 0.1
+particlesMaterial.sizeAttenuation = true
+//particlesMaterial.color = new THREE.Color("#ff88cc")
+particlesMaterial.transparent =true
+particlesMaterial.alphaMap = particleTexture
+//particlesMaterial.alphaTest = 0.001
+//particlesMaterial.depthTest = false
+particlesMaterial.depthWrite = false
+particlesMaterial.blending = THREE.AdditiveBlending
+particlesMaterial.vertexColors = true
 
-const torus = new THREE.Mesh(
-    new THREE.TorusBufferGeometry(0.3,0.2,16,32),
-    material
-)
-torus.position.x = 1.5
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
 
-scene.add(sphere, plane, torus)
-
-const ambientLight = new THREE.AmbientLight(0xffffff, 0,5)
-scene.add(ambientLight)
-
-const pointLight = new THREE.PointLight(0xffffff,0.5)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-
-scene.add(pointLight)
+// const cube = new THREE.Mesh(
+//     new THREE.BoxBufferGeometry(),
+//     new THREE.MeshBasicMaterial()
+// )
+// scene.add(cube)
 
 /**
  * Sizes
@@ -97,9 +92,7 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 1
-camera.position.y = 1
-camera.position.z = 2
+camera.position.z = 3
 scene.add(camera)
 
 // Controls
@@ -124,14 +117,18 @@ const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
-    // Update objects
-    sphere.rotation.y = elapsedTime * 0.1
-    plane.rotation.y = elapsedTime * 0.1
-    torus.rotation.y = elapsedTime * 0.1
+    // Update particles
+    //particles.rotation.y = elapsedTime * 0.2
 
-    sphere.rotation.x = elapsedTime * 0.15
-    plane.rotation.x = elapsedTime * 0.15
-    torus.rotation.x = elapsedTime * 0.15
+    for(let i=-0; i < count; i++) {
+        const i3 = i * 3
+        //x
+        const x = particlesGeometry.attributes.position.array[i3]
+        //y
+        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x)
+    }
+
+    particlesGeometry.attributes.position.needsUpdate = true
 
     // Update controls
     controls.update()
